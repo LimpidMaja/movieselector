7f8bbf72b40a2d4905e06bc24060e0a4a5cb397d
LimpidMaja <dobnik.maja@gmail.com>
2014-10-11 00:39:46 +0200

    update migration

--- db/migrate/20141007160739_change_events.rb
+++ db/migrate/20141007160739_change_events.rb
@@ -1,13 +1,13 @@
 class ChangeEvents < ActiveRecord::Migration
   def up
-    change_column :events, :rating_system, :integer, default: 0 
-    change_column :events, :voting_range, :integer, default: 0 
+    change_column :events, :rating_system, :integer 
+    change_column :events, :voting_range, :integer 
     add_column   :events, :rating_phase, :integer, default: 0 
       
   end
   
   def down      
-    remove_column :events, :rating_phase, :integer, default: 0
+    remove_column :events, :rating_phase, :integer
     change_column :events, :rating_system, :string
     change_column :events, :voting_range, :string
   end
