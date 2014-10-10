db087beab502a89b2f636e3b00c31431e4f82281
LimpidMaja <dobnik.maja@gmail.com>
2014-10-11 01:02:50 +0200

    update migration

--- db/migrate/20141007160739_change_events.rb
+++ db/migrate/20141007160739_change_events.rb
@@ -1,8 +1,20 @@
 class ChangeEvents < ActiveRecord::Migration
   def up
-    change_column :events, :rating_system, :integer 
-    change_column :events, :voting_range, :integer 
+    #change_column :events, :rating_system, :integer 
+    #change_column :events, :voting_range, :integer 
     add_column   :events, :rating_phase, :integer, default: 0 
+    
+    connection.execute(%q{
+      alter table events
+      alter column rating_system
+      type integer using cast(string as integer)
+    })
+    
+    connection.execute(%q{
+      alter table events
+      alter column voting_range
+      type integer using cast(string as integer)
+    })
       
   end
   
