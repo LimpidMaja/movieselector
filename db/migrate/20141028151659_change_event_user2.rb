0da18f3466de3f4df92e5a7dcce74a931a8202b2
LimpidMaja <dobnik.maja@gmail.com>
2014-11-01 20:41:35 +0100

    db migrate fix

--- db/migrate/20141028151659_change_event_user2.rb
+++ db/migrate/20141028151659_change_event_user2.rb
@@ -1,12 +1,12 @@
 class ChangeEventUser2 < ActiveRecord::Migration
   def up
     remove_column :event_users, :accept, :integer, default: 0
-    add_column   :event_users, :accept, :boolean, default: 0  
+    add_column   :event_users, :accept, :boolean, default: false  
       
   end
   
   def down     
-    remove_column :event_users, :accept, :boolean, default: 0
+    remove_column :event_users, :accept, :boolean, default: false
     add_column   :event_users, :accept, :integer, default: 0  
   end
 end
--- db/migrate/20141028155719_change_event_user3.rb
+++ db/migrate/20141028155719_change_event_user3.rb
@@ -1,13 +1,13 @@
 class ChangeEventUser3 < ActiveRecord::Migration
    def up
-    remove_column :event_users, :accept, :boolean, default: 0    
+    remove_column :event_users, :accept, :boolean, default: false    
     add_column   :event_users, :accept, :integer, default: 0  
       
   end
   
   def down      
     remove_column :event_users, :accept, :integer, default: 0
-    add_column   :event_users, :accept, :boolean, default: 0  
+    add_column   :event_users, :accept, :boolean, default: false  
        
   end
 end
